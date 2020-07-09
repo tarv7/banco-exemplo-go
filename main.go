5b4285abe8a498a99528624a6a8039a5e1a2788e
tarv7 <thales.ramos@enjoei.com.br>
2020-07-09 15:45:44 -0300

    Adicionando Conta Poupança e pagamento de boleto

--- main.go
+++ main.go
@@ -6,10 +6,27 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	clienteThales := clientes.Titular{"Thales", "123.111.123.12", "Programador"}
 	contaDoThales := contas.ContaCorrente{Titular: clienteThales, NumeroAgencia: 123, NumeroConta: 123456}
 	contaDoThales.Depositar(100)
 
-	fmt.Println(contaDoThales.ObterSaldo())
+	PagarBoleto(&contaDoThales, 60)
+
+	fmt.Println(contaDoThales)
+
+	contaDoArthur := contas.ContaPoupanca{}
+	contaDoArthur.Depositar(320)
+
+	PagarBoleto(&contaDoArthur, 120)
+
+	fmt.Println(contaDoArthur)
 }
